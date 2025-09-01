b34ede8322f04e15a3c247067266286bfc484440
devjuliomartins <contatoeujuliomartins@gmail.com>
2025-09-01 13:28:44 -0300

    refactor: marca campo categoriaService como final

--- src/main/java/br/com/juliomartins/VassCommerce/controller/CategoriaController.java
+++ src/main/java/br/com/juliomartins/VassCommerce/controller/CategoriaController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RequestMapping("/categoria")
 public class CategoriaController {
 
-    private CategoriaService categoriaService;
+    private final CategoriaService categoriaService;
 
     public CategoriaController(CategoriaService categoriaService) {
         this.categoriaService = categoriaService;

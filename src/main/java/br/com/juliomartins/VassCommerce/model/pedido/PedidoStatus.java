ae17a296281917e1aa339d4231001443ea262d7b
devjuliomartins <contatoeujuliomartins@gmail.com>
2025-09-07 19:40:07 -0300

    feat: adição classe/enum de Pedido

---
+++ src/main/java/br/com/juliomartins/VassCommerce/model/pedido/PedidoStatus.java
@@ -0,0 +1,8 @@
+package br.com.juliomartins.VassCommerce.model.pedido;
+
+public enum PedidoStatus {
+    AGUARDANDO_PAGAMENTO,
+    SEPARANDO_ESTOQUE,
+    ESTREGUE_TRANSPORTADORA,
+    ENTREGUE_CLIENTE;
+}

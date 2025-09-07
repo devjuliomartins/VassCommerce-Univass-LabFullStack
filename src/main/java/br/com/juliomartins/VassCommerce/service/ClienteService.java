21b807c2a6aa0fed017893afd70b63154a175ce5
devjuliomartins <contatoeujuliomartins@gmail.com>
2025-09-07 19:38:22 -0300

    feat(mock): add mock de Pedidos a Cliente

--- src/main/java/br/com/juliomartins/VassCommerce/service/ClienteService.java
+++ src/main/java/br/com/juliomartins/VassCommerce/service/ClienteService.java
@@ -1,14 +1,20 @@
 package br.com.juliomartins.VassCommerce.service;
 
 import br.com.juliomartins.VassCommerce.model.Cliente;
+import br.com.juliomartins.VassCommerce.model.Produto;
 import br.com.juliomartins.VassCommerce.model.endereco.Cidade;
 import br.com.juliomartins.VassCommerce.model.endereco.Endereco;
 import br.com.juliomartins.VassCommerce.model.endereco.Estado;
 import br.com.juliomartins.VassCommerce.model.formasdepagamento.Cartao;
 import br.com.juliomartins.VassCommerce.model.formasdepagamento.FormasDePagamento;
 import br.com.juliomartins.VassCommerce.model.formasdepagamento.TipoCartao;
+import br.com.juliomartins.VassCommerce.model.pedido.ItemPedido;
+import br.com.juliomartins.VassCommerce.model.pedido.Pedido;
+import br.com.juliomartins.VassCommerce.model.pedido.PedidoStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.*;
 
@@ -29,6 +35,14 @@ public class ClienteService {
         Endereco endereco = new Endereco(1, "Rua A", 123, "01000-000", "", "11999999999",
                 "Centro", new Date(), new Date(), rioDeJaneiro);
 
+
+        ProdutoService produtoService = new ProdutoService();
+        Produto notebook = produtoService.buscarProdutoId(1);
+        ItemPedido itemPedido1 = new ItemPedido(1, 1, notebook.getValorUnitario(), notebook);
+        Pedido pedido1 = new Pedido(1, new Date(),
+                itemPedido1.getValorUnitario().multiply(new BigDecimal(itemPedido1.getQuantidade())),
+                PedidoStatus.AGUARDANDO_PAGAMENTO, itemPedido1);
+
         cliente.add(new Cliente(
                 1,
                 "Julio Fernando",
@@ -39,7 +53,8 @@ public class ClienteService {
                 dataNascimento,
                 "1234567",
                 formasDePagamentos,
-                endereco
+                endereco,
+                pedido1
         ));
     }
 

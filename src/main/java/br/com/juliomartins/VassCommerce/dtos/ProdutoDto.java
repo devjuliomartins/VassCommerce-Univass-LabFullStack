ca67667d1f49e5e132f1fa09a75d949b692a16d6
devjuliomartins <contatoeujuliomartins@gmail.com>
2025-09-13 17:30:30 -0300

    feat: criar ProdutoDto

---
+++ src/main/java/br/com/juliomartins/VassCommerce/dtos/ProdutoDto.java
@@ -0,0 +1,52 @@
+package br.com.juliomartins.VassCommerce.dtos;
+
+import java.math.BigDecimal;
+
+public class ProdutoDto {
+    private String nome;
+    private String descricao;
+    private String fotoUrl;
+    private BigDecimal valorUnitario;
+    private Long idCategoria;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getFotoUrl() {
+        return fotoUrl;
+    }
+
+    public void setFotoUrl(String fotoUrl) {
+        this.fotoUrl = fotoUrl;
+    }
+
+    public BigDecimal getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(BigDecimal valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public Long getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Long idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+}
+

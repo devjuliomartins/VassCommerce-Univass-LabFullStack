51313b7b478858eb26ae133f164080503e897fc6
devjuliomartins <contatoeujuliomartins@gmail.com>
2025-09-13 17:23:26 -0300

    feat: criar repository de Produto

---
+++ src/main/java/br/com/juliomartins/VassCommerce/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package br.com.juliomartins.VassCommerce.repository;
+
+import br.com.juliomartins.VassCommerce.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    List<Produto> findByCategoria_Id(Long idCategoria);
+}

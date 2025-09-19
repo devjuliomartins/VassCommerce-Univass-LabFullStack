f707f056ed63523ab831580a954aaf437ee1731a
devjuliomartins <contatoeujuliomartins@gmail.com>
2025-09-19 13:24:30 -0300

    refactor: adição endpoint listar todos produtos

--- src/main/java/br/com/juliomartins/VassCommerce/controller/ProdutoController.java
+++ src/main/java/br/com/juliomartins/VassCommerce/controller/ProdutoController.java
@@ -2,8 +2,8 @@ package br.com.juliomartins.VassCommerce.controller;
 
 import br.com.juliomartins.VassCommerce.dtos.ProdutoDto;
 import br.com.juliomartins.VassCommerce.model.Produto;
-import br.com.juliomartins.VassCommerce.repository.CategoriaRepository;
-import br.com.juliomartins.VassCommerce.repository.ProdutoRepository;
+import br.com.juliomartins.VassCommerce.service.CategoriaService;
+import br.com.juliomartins.VassCommerce.service.ProdutoService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,79 +16,15 @@ import java.util.Optional;
 @RequestMapping("/produto")
 public class ProdutoController {
 
-    private final ProdutoRepository repository;
-    private CategoriaRepository categoriaRepository;
+    private final ProdutoService produtoService;
 
-    public ProdutoController(ProdutoRepository repository, CategoriaRepository categoriaRepository) {
-        this.repository = repository;
-        this.categoriaRepository = categoriaRepository;
-
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
     }
 
-    // POST /produto → Criar Produto
-    @PostMapping
-    public ResponseEntity save(@RequestBody ProdutoDto dto) {
-        var produto = new Produto();
-        BeanUtils.copyProperties(dto, produto);
-
-        var categoria = categoriaRepository.findById(dto.getIdCategoria())
-                .orElseThrow(() -> new RuntimeException("Categoria não encontrada"));
-
-        produto.setCategoria(categoria);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
-    }
-
-    // GET /produto/{id} → Buscar produto pelo ID
-    @GetMapping("/{id}")
-    public ResponseEntity getById(@PathVariable(value = "id") Long id) {
-        Optional<Produto> produto = repository.findById(id);
-
-        if (produto.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado!");
-        }
-
-        return ResponseEntity.status(HttpStatus.OK).body(produto.get());
-    }
-
-    // GET /produto → Listar todos os produtos
-    @GetMapping
-    public ResponseEntity getAll() {
-        List<Produto> listarProdutos = repository.findAll();
-        return ResponseEntity.status(HttpStatus.OK).body(listarProdutos);
-    }
-
-    // PUT /produto/{id} → Atualizar produto
-    @PutMapping("/{id}")
-    public ResponseEntity update(@PathVariable(value = "id") Long id, @RequestBody ProdutoDto dto) {
-        Optional<Produto> produto = repository.findById(id);
-
-        if (produto.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado!");
-        }
-
-        var produtoModel = produto.get();
-        BeanUtils.copyProperties(dto, produtoModel);
-        return ResponseEntity.status(HttpStatus.OK).body(repository.save(produtoModel));
-    }
-
-    // DELETE /produto/{id} → Deletar produto
-    @DeleteMapping("/{id}")
-    public ResponseEntity delete(@PathVariable(value = "id") Long id) {
-        Optional<Produto> produto = repository.findById(id);
-
-        if (produto.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado!");
-        }
-
-        repository.delete(produto.get());
-        return ResponseEntity.status(HttpStatus.OK).body("Produto deletado com sucesso!");
-    }
-
-    // GET /categoria/{idCategoria}/produto → Listagem de todos os produtos da categoria
-    @GetMapping("/categoria/{idCategoria}")
-    public ResponseEntity listarProdutosCategoria(@PathVariable Long idCategoria) {
-        List<Produto> produtos = repository.findByCategoria_Id(idCategoria);
-        return ResponseEntity.status(HttpStatus.OK).body(produtos);
+    // GET /produto → listar todos os produtos
+    @GetMapping()
+    public List<Produto> listarTodosProdutos() {
+        return produtoService.listarTodos();
     }
 }

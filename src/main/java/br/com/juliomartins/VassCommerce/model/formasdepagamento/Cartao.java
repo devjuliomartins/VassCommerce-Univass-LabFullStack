50c9ab8aed2a2165593f270092f63a199b420908
devjuliomartins <contatoeujuliomartins@gmail.com>
2025-09-23 10:28:04 -0300

    refactor: reorganizaçao da classe Cartao

--- src/main/java/br/com/juliomartins/VassCommerce/model/formasdepagamento/Cartao.java
+++ src/main/java/br/com/juliomartins/VassCommerce/model/formasdepagamento/Cartao.java
@@ -2,18 +2,55 @@ package br.com.juliomartins.VassCommerce.model.formasdepagamento;
 
 import java.util.Date;
 
-public class Cartao extends FormasDePagamento {
-    private TipoCartao tipoCartao;
+public class Cartao {
+    private Long id;
+    private Long idCliente;
+    private int tipoCartao;
+    private Date dataCriacao;
+    private Boolean excluido;
 
-    public Cartao(long id, Date dataCricao, boolean exluido, TipoCartao tipoCartao) {
-        super(id, dataCricao, exluido);
+    public Cartao() {}
+
+    public Cartao(Long id, Long idCliente, int tipoCartao, Date dataCriacao, Boolean excluido) {
+        this.id = id;
+        this.idCliente = idCliente;
         this.tipoCartao = tipoCartao;
+        this.dataCriacao = dataCriacao;
+        this.excluido = excluido;
     }
 
-    public TipoCartao getTipoCartao() {
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdCliente() {
+        return idCliente;
+    }
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getTipoCartao() {
         return tipoCartao;
     }
-    public void setTipoCartao(TipoCartao tipoCartao) {
+    public void setTipoCartao(int tipoCartao) {
         this.tipoCartao = tipoCartao;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Boolean getExcluido() {
+        return excluido;
+    }
+    public void setExcluido(Boolean excluido) {
+        this.excluido = excluido;
     }
 }

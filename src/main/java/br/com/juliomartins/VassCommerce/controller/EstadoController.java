51ecc2e183ceadc4772608a2fe98e6b02d6ca495
devjuliomartins <contatoeujuliomartins@gmail.com>
2025-09-23 09:46:42 -0300

    feat: adição do endpoint listar Estados

--- src/main/java/br/com/juliomartins/VassCommerce/controller/EstadoController.java
+++ src/main/java/br/com/juliomartins/VassCommerce/controller/EstadoController.java
@@ -1,36 +1,27 @@
-//package br.com.juliomartins.VassCommerce.controller;
-//
-//import br.com.juliomartins.VassCommerce.model.endereco.Cidade;
-//import br.com.juliomartins.VassCommerce.model.endereco.Estado;
-//import br.com.juliomartins.VassCommerce.service.EstadoService;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping
-//public class EstadoController {
-//
-//    private final EstadoService estadoService;
-//
-//    public EstadoController(EstadoService estadoService) {
-//        this.estadoService = estadoService;
-//    }
-//
-//
-////  Get /estado → Listar todos os Estados
-//    @GetMapping("/estado")
-//    public List<Estado> listarEstados() {
-//        return estadoService.todosEstados();
-//    }
-//
-////  Get /estado/{idEstado}/cidade → Listar todas Cidades de um Estado
-//    @GetMapping("/estado/{idEstado}/cidade")
-//    public List<Cidade> listarCidadesEstado(@PathVariable long idEstado) {
-//        return estadoService.todasCidadesEstado(idEstado);
-//    }
-//
-//}
+package br.com.juliomartins.VassCommerce.controller;
+
+import br.com.juliomartins.VassCommerce.model.endereco.Estado;
+import br.com.juliomartins.VassCommerce.service.EstadoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estado")
+public class EstadoController {
+
+    private final EstadoService estadoService;
+
+    public EstadoController(EstadoService estadoService) {
+        this.estadoService = estadoService;
+    }
+
+
+    //  GET /estado → Listar todos os Estados
+    @GetMapping
+    public List<Estado> listarEstados() {
+        return estadoService.listarEstados();
+    }
+}
--- src/main/java/br/com/juliomartins/VassCommerce/service/EstadoService.java
+++ src/main/java/br/com/juliomartins/VassCommerce/service/EstadoService.java
@@ -1,41 +1,49 @@
-//package br.com.juliomartins.VassCommerce.service;
-//
-//import br.com.juliomartins.VassCommerce.model.Cliente;
-//import br.com.juliomartins.VassCommerce.model.endereco.Cidade;
-//import br.com.juliomartins.VassCommerce.model.endereco.Endereco;
-//import br.com.juliomartins.VassCommerce.model.endereco.Estado;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.bind.annotation.PathVariable;
-//
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Service
-//public class EstadoService {
-//
-//    private final List<Cliente> cliente;
-//
-//    public EstadoService(ClienteService clienteService) {
-//        this.cliente = clienteService.getCliente();
-//    }
-//
-//    // Listar todos Estados
-//    public List<Estado> todosEstados() {
-//        return cliente.stream()
-//                .map(Cliente::getEndereco)
-//                .map(Endereco::getCidade)
-//                .map(Cidade::getEstado)
-//                .distinct()
-//                .collect(Collectors.toList());
-//    }
-//
-//    // Listar todas Cidades de um Estado
-//    public List<Cidade> todasCidadesEstado(@PathVariable long idEstado) {
-//        return cliente.stream()
-//                .map(Cliente::getEndereco)
-//                .map(Endereco::getCidade)
-//                .filter(cidade -> cidade.getEstado().getId() == idEstado)
-//                .distinct()
-//                .collect(Collectors.toList());
-//    }
-//}
+package br.com.juliomartins.VassCommerce.service;
+
+import br.com.juliomartins.VassCommerce.model.endereco.Estado;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EstadoService {
+
+    private final List<Estado> estados = new ArrayList<>();
+
+    public EstadoService(ClienteService clienteService) {
+        estados.add(new Estado(1L, "AC", "Acre"));
+        estados.add(new Estado(2L, "AL", "Alagoas"));
+        estados.add(new Estado(3L, "AP", "Amapá"));
+        estados.add(new Estado(4L, "AM", "Amazonas"));
+        estados.add(new Estado(5L, "BA", "Bahia"));
+        estados.add(new Estado(6L, "CE", "Ceará"));
+        estados.add(new Estado(7L, "DF", "Distrito Federal"));
+        estados.add(new Estado(8L, "ES", "Espírito Santo"));
+        estados.add(new Estado(9L, "GO", "Goiás"));
+        estados.add(new Estado(10L, "MA", "Maranhão"));
+        estados.add(new Estado(11L, "MT", "Mato Grosso"));
+        estados.add(new Estado(12L, "MS", "Mato Grosso do Sul"));
+        estados.add(new Estado(13L, "MG", "Minas Gerais"));
+        estados.add(new Estado(14L, "PA", "Pará"));
+        estados.add(new Estado(15L, "PB", "Paraíba"));
+        estados.add(new Estado(16L, "PR", "Paraná"));
+        estados.add(new Estado(17L, "PE", "Pernambuco"));
+        estados.add(new Estado(18L, "PI", "Piauí"));
+        estados.add(new Estado(19L, "RJ", "Rio de Janeiro"));
+        estados.add(new Estado(20L, "RN", "Rio Grande do Norte"));
+        estados.add(new Estado(21L, "RS", "Rio Grande do Sul"));
+        estados.add(new Estado(22L, "RO", "Rondônia"));
+        estados.add(new Estado(23L, "RR", "Roraima"));
+        estados.add(new Estado(24L, "SC", "Santa Catarina"));
+        estados.add(new Estado(25L, "SP", "São Paulo"));
+        estados.add(new Estado(26L, "SE", "Sergipe"));
+        estados.add(new Estado(27L, "TO", "Tocantins"));
+    }
+
+    // Listar todos Estados
+    public List<Estado> listarEstados() {
+        return estados;
+    }
+
+}

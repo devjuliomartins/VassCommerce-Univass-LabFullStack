de1b5f626503fa6418d3c34f19f160feecd03a77
devjuliomartins <contatoeujuliomartins@gmail.com>
2025-09-23 09:30:59 -0300

    feat: adição endpoint atualizar Cliente

--- src/main/java/br/com/juliomartins/VassCommerce/controller/ClienteController.java
+++ src/main/java/br/com/juliomartins/VassCommerce/controller/ClienteController.java
@@ -1,41 +1,28 @@
-//package br.com.juliomartins.VassCommerce.controller;
-//
-//import br.com.juliomartins.VassCommerce.model.Cliente;
-//import br.com.juliomartins.VassCommerce.model.endereco.Endereco;
-//import br.com.juliomartins.VassCommerce.model.formasdepagamento.FormasDePagamento;
-//import br.com.juliomartins.VassCommerce.service.ClienteService;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping
-//public class ClienteController {
-//    private final ClienteService clienteService;
-//
-//    public ClienteController(ClienteService clienteService) {
-//        this.clienteService = clienteService;
-//    }
-//
-//
-////  GET /cliente/{id} → Visualizar dados do Cliente
-//    @GetMapping("/cliente/{id}")
-//    public Cliente VisualizarDadosCliente(@PathVariable long id) {
-//        return clienteService.dadosCliente(id);
-//    }
-//
-////  GET /cliente/{id}/forma-de-pagamento → Lista todas as formas de pagamento do Cliente
-//    @GetMapping("/cliente/{id}/forma-de-pagamento")
-//    public List<FormasDePagamento> VisualizarFormasDePagamentos(@PathVariable long id) {
-//        return clienteService.formasDePagamentosCliente(id);
-//    }
-//
-////  Get /cliente/{idCliente}/endereco → Busca pelos dados do endereço do cliente
-//    @GetMapping("/cliente/{id}/endereco")
-//    public Endereco VisualizarEnderecosCliente(@PathVariable long id) {
-//        return clienteService.listarEnderecoCliente(id);
-//    }
-//
-//}
+package br.com.juliomartins.VassCommerce.controller;
+
+import br.com.juliomartins.VassCommerce.model.Cliente;
+import br.com.juliomartins.VassCommerce.service.ClienteService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+
+    //  GET /cliente/{id} → Visualizar dados do Cliente
+    @GetMapping("/{id}")
+    public Cliente visualizarDadosCliente(@PathVariable long id) {
+        return clienteService.visualizarDadosCliente(id);
+    }
+
+    //  PUT /cliente/{id} → Atualizar dados do Cliente
+    @PutMapping("/{id}")
+    public Cliente atualizarDadosClienteup(@PathVariable Long id, @RequestBody Cliente cliente) {
+        return clienteService.update(id, cliente);
+    }
+}

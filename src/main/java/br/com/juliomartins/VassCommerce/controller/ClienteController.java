d1c019530b689b21050860e48ded729590c459a9
devjuliomartins <contatoeujuliomartins@gmail.com>
2025-09-24 09:58:23 -0300

    feat: adição do endpoint visualizar Pedidos cliente com filtro

--- src/main/java/br/com/juliomartins/VassCommerce/controller/ClienteController.java
+++ src/main/java/br/com/juliomartins/VassCommerce/controller/ClienteController.java
@@ -1,16 +1,24 @@
 package br.com.juliomartins.VassCommerce.controller;
 
 import br.com.juliomartins.VassCommerce.model.Cliente;
+import br.com.juliomartins.VassCommerce.model.pedido.Pedido;
 import br.com.juliomartins.VassCommerce.service.ClienteService;
+import br.com.juliomartins.VassCommerce.service.PedidoService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
-    private final ClienteService clienteService;
 
-    public ClienteController(ClienteService clienteService) {
+    private final ClienteService clienteService;
+    private final PedidoService pedidoService;
+
+    public ClienteController(ClienteService clienteService, PedidoService pedidoService) {
         this.clienteService = clienteService;
+        this.pedidoService = pedidoService;
     }
 
 
@@ -30,5 +38,24 @@ public class ClienteController {
     @DeleteMapping(value = "/{id}", produces = "application/json")
     public void deletarCliente(@PathVariable Long id) {
         clienteService.delete(id);
+    }
+
+    //  GET /cliente/{idCliente}/pedido → Visualizar Pedidos do cliente
+    @GetMapping(value = "/{idCliente}/pedido", produces = "application/json")
+    public List<Pedido> listarPedidos(
+            @PathVariable Long idCliente,
+            @RequestParam(required = false) String datainicio,
+            @RequestParam(required = false) String datafinal
+    ) {
+        if (datainicio != null && datafinal != null) {
+            try {
+                Date inicio = new Date(Long.parseLong(datainicio));
+                Date fim = new Date(Long.parseLong(datafinal));
+                return pedidoService.listarPedidosPorPeriodo(idCliente, inicio, fim);
+            } catch (NumberFormatException e) {
+                throw new RuntimeException("Datas devem ser em timestamp (ms)");
+            }
+        }
+        return pedidoService.listarPedidosPorCliente(idCliente);
     }
 }
